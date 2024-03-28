b22cfc7098f0d73accddc006643bc7b2d7fec76a
EPPnathan <nathan.gazin@efrei.net>
2024-03-28 14:44:46 +0100

    nn

--- fonctions.h
+++ fonctions.h
@@ -1,6 +1,15 @@
 #ifndef CDATAFRAME_FONCTIONS_H
 #define CDATAFRAME_FONCTIONS_H
 
+#define REALOC_SIZE 256
 
+typedef struct{
+    char titre[20];
+    int taille_physique;
+    int taille_logique;
+    int donnees[REALOC_SIZE];
+}COLUMN;
+
+COLUMN *create_column(char* title);
 
 #endif
