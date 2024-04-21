adda61fb298c3c45c90cb717e1c1366b7a580520
EPPnathan <151038263+EPPnathan@users.noreply.github.com>
2024-04-21 13:36:33 +0200

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -5,16 +5,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+
+
+
 typedef struct{
     char titre[20];
     int taille_physique;
     int taille_logique;
-    int* donnees;
+    int * donnees;
 }COLUMN;
 
-COLUMN *create_column(char* title);
-int insert_value( COLUMN * mycol, int val );
-void delete_column(COLUMN *col);
-void print_col(COLUMN* col);
+
+COLUMN * create_column(char* title); /** Crée une colonne **/
+int insert_value( COLUMN * mycol, int val ); /** Insert une valeur dans une colonne **/
+void delete_column(COLUMN *col); /** Supprimme une colonne **/
+void print_col(COLUMN* col); /** Affiche une colonne **/
+int nb_occurrences(COLUMN *col, int val); /** Renvoi le nombre d'occurence d'une valeur dans une colonne **/
+int val_precedent(COLUMN *col, int pos); /** Renvoi la valeur précédente à un indice donné dans une colonne**/
+int nb_val_sup(COLUMN *col, int val); /** Renvoi le nombre de valeur supérieur à une valeur dans une colonne **/
+int nb_val_inf(COLUMN *col, int val); /** Renvoi le nombre de valeur inférieur à une valeur dans une colonne **/
+
+
+COLUMN ** create_CDataframe(); /** Crée un CDataframe **/
+void saisie_CDataframe( COLUMN ** cd, int taille_tab); /** Permet de remplir un CDataframe a clavier **/
+COLUMN ** saisie_dur_CDataframe(COLUMN ** cd); /** Permet de remplir un CDataframe en dur **/
+void print_CDataframe(COLUMN ** cd, int taille);
+void print_colones_x(COLUMN ** cd, int taille, int val);
+void print_lignes_x(COLUMN ** cd, int taille, int val);
+void ajout_ligne_CDataframe(COLUMN ** cd, int taille);
 
 #endif
